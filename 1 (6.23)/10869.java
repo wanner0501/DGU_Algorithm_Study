864d2038f6abd513183efc88e4c27e2ff8495448
wanner0501 <32871449+wanner0501@users.noreply.github.com>
2024-06-24 19:09:11 +0900

    10869 제출

---
+++ 1 (6.23)/10869.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}
+}
